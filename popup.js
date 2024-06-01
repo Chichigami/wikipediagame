5b0e0d981647fde18799bbd97b011f375bffacd4
Chichigami <g.feng.work@gmail.com>
2024-06-01 17:29:41 -0400

    url getter

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
+var url = 'https://raw.githubusercontent.com/Chichigami/wikipediagame/main/dailylink.txt';
+
+fetch(url)
+  .then(response => {
+    return response.text();
+  })
+  .then(data => { 
+    console.log(data[start]);
+  })
+
+
 document.getElementById('navigateButton').addEventListener('click', () => {
     chrome.tabs.create({ url: 'https://en.wikipedia.org/wiki/Special:Random' });
 });
